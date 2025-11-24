fef5096351f12435e9a19ff7b1a31b0f7d398f6d
Manuel Horeth <mhoreth@htl-steyr.ac.at>
2025-11-24 13:33:31 +0100

    added talk and burp method to MHoreth.java and LKreisma.java

--- src/LKreisma.java
+++ src/LKreisma.java
@@ -3,5 +3,11 @@ public class lkreisma implements TellYourName {
     public void tell(){
         System.out.println("LKreisma...");
     }
+    public void burp(){
+        System.out.println("burp....");
+    }
+    public void talk(){
+        System.out.println("bla bla bla");
+    }
 
 }
